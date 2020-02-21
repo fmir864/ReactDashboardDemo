7a97f9311498a52cf4faa15d0ce329ff8d87cf70
FM Irshad <fmir864@outlook.com>
2020-02-21 13:44:13 +0530

    Server-side Data

--- ReactDashboardDemo/Controllers/HomeController.cs
+++ ReactDashboardDemo/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ReactDashboardDemo.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,6 +11,34 @@ namespace ReactDashboardDemo.Controllers
 {
     public class HomeController : Controller
     {
+        [Route("comments")]
+        [ResponseCache(Location = ResponseCacheLocation.None, NoStore = true)]
+        public ActionResult Comments()
+        {
+            List<CommentModel> _comments = new List<CommentModel>
+            {
+                new CommentModel
+                {
+                    Id = 1,
+                    Author = "Daniel Lo Nigro",
+                    Text = "Hello ReactJS.NET World!"
+                },
+                new CommentModel
+                {
+                    Id = 2,
+                    Author = "Pete Hunt",
+                    Text = "This is one comment"
+                },
+                new CommentModel
+                {
+                    Id = 3,
+                    Author = "Jordan Walke",
+                    Text = "This is *another* comment"
+                },
+            };
+            return Json(_comments);
+        }
+
         // GET: /<controller>/
         public IActionResult Index()
         {
